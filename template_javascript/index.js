e4b4b62c68bafada16022cfa57be6419d1e05c1a
Felipe Muriel Machado Ortiz <165331761+FelipeMurielMachadoOrtiz@users.noreply.github.com>
2024-04-11 10:54:35 -0300

    introducao-com-pompt

---
+++ template_javascript/index.js
@@ -0,0 +1,22 @@
+/*
+const primeironome = 'Felipe.';
+const sobrenome = 'Ortiz.';
+const idade = 15;
+const estudande = 'Sim.';
+const partido = 'PT/Comunista.'
+console.log('Meu primeiro nome é:',primeironome,'Meu sobrenome é:', sobrenome,'Minha idade é:', idade,'anos',
+'Estudante:',estudande, 'Meu partido é:',partido);
+console.log('E sim eu faço o L')
+*/
+/*
+let nome = prompt('qual é seu nome?')
+let sobrenome = prompt('qual é seu sobrenome?')
+let idade = prompt('qual é sua idade?')
+alert(`seu nome é ${nome} seu sobrenome é ${sobrenome} sua idade é ${idade}`)
+*/
+
+/*
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+*/
