e33000a0cd2f000b69c4b7b72dd908bb132167ca
Felipe Muriel Machado Ortiz <165331761+FelipeMurielMachadoOrtiz@users.noreply.github.com>
2024-04-17 09:36:34 -0300

    aulas_logica

--- aula_do_dia_16/index.js
+++ aula_do_dia_16/index.js
@@ -1,4 +1,5 @@
-//DECLARAÇAO DAS VARIAVEIS
+
+/*//DECLARAÇAO DAS VARIAVEIS
 let nome 
 let idade 
 let anoNascimento
@@ -21,3 +22,28 @@ alert(nome)
 alert(idade)
 alert(maiorIdade)
 alert(idade2050)
+*/
+let nome
+
+nome = prompt("Digite seu nome:")
+
+let anoAtual
+let anoNascimento
+let idade
+
+anoAtual = prompt("Digite o ano atual:")
+anoNascimento = prompt("Digite o ano de nascimento:")
+idade = anoAtual - anoNascimento
+alert(idade)
+
+let maiorIdade
+
+maiorIdade = idade >= 18
+alert(maiorIdade)
+
+let idade2050
+
+idade2050= 2050 - anoNascimento
+alert(idade2050)
+
+console.log("Oi!!" , "Meu nome é " , nome, "e eu tenho", idade, "anos" , "eu sou maior de idade" , maiorIdade, "e em 2050, terei", idade2050, "anos")
