ef232b1ba4f003f9296b00b59ee4db45dd359115
Felipe Muriel Machado Ortiz <165331761+FelipeMurielMachadoOrtiz@users.noreply.github.com>
2024-04-22 09:21:49 -0300

    dia_18

---
+++ aula_dia_18/index.js
@@ -0,0 +1,50 @@
+//let nome = prompt("Digite seu nome:")
+//let cor = prompt("Digite qual é sua cor favorita:")
+
+//alert("A cor favorita do "  +  nome  +  " é "  +  cor)
+
+//alert(`A cor favorita do ${nome} é ${cor}`)
+
+//let frase = "o Luffy vai ser o rei dos piratas"
+
+//let fraseMaiuscula = frase.toLocaleUpperCase()
+//console.log(fraseMaiuscula)
+
+//let novaFrase = frase.replaceAll("o", "i")
+//console.log(novaFrase)
+
+//console.log(frase.length)
+
+//const frase = prompt("Digite uma frase:")
+
+//let fraseMaiuscula = frase.toLocaleUpperCase()
+//console.log(fraseMaiuscula)
+
+//let novaFrase = frase.replaceAll("o", "i")
+//console.log(novaFrase)
+//ERRADO
+//console.log(frase.length)
+
+/*const racasDeCaes = ["vira-lata", "caramelo", "pitbull", "salcicha", "pastor-alemão"]
+const viraLata = racasDeCaes[0]
+const caramelo = racasDeCaes[1]
+const pitbull = racasDeCaes[2]
+const salsicha = racasDeCaes[3]
+const pastorAlemao = racasDeCaes[4]
+console.log(racasDeCaes)
+console.log(viraLata)
+console.log(caramelo)
+console.log(pitbull)
+console.log(salsicha)
+console.log(pastorAlemao)*/
+
+//const racasDeCaes = ["vira-lata", "caramelo", "pitbull", "salcicha", "pastor-alemão"]
+//let escolher = prompt("Digite um numero de 0 a 4:")
+//alert(racasDeCaes[escolher])
+
+/*const liste = [1,2,3,4,5,6]
+alert(liste.length)
+liste.push(7)
+alert(liste)
+liste.splice(4,5)
+alert(liste)*/
