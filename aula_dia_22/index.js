7bd864530af218346c634576bd59816ef4bb130a
Felipe Muriel Machado Ortiz <165331761+FelipeMurielMachadoOrtiz@users.noreply.github.com>
2024-04-23 11:53:18 -0300

    aula_dia_22

---
+++ aula_dia_22/index.js
@@ -0,0 +1,38 @@
+/*let array
+console.log('a. ', array)//vai imprimir a palavra "a." no console e deóis vai dizer o tipo da array que é undefiled
+
+array = null
+console.log('b. ', array)//vai imprimir a palavra "b." e vai dá o tipo do array depois, que é null
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)//vai imprimir no console "c." e depois vai dá a quantidade de itens no array
+
+let i = 0
+console.log('d. ', array[i])//vai imprimi no console "d." e vai dá o valor do item dentro da casa 0
+
+array[i+1] = 19
+console.log('e. ', array)//vai imprimi no console "e." e depois vai imprimi a array que passa ter o valor 19 no lugar do 4
+
+const valor = array[i+6]
+console.log('f. ', valor)//vai imprimi "f." e depois vai imprimir a vaiavel valor que é = 3 + 6 por causa das casas da array
+
+const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)// "Subi num ônibus em Marrocos" essa frase vai ser imprimida "SUBI NUM ÔNIBUS EM MIRROCOS" com a quantidade de itens na frase
+*/
+
+/*let emailDoUsuario = prompt("Digite seu email:")
+let nomeDoUsuario = prompt("Digite seu nome:")
+console.log(`O email ${emailDoUsuario} foi cadastrado com sucesso.Seja bem-vinda(o), ${nomeDoUsuario}`)*/
+
+/*let arrayComidas = ["Strogonoff", "Pizza", "Hamburguer", "Batata Frita", "Kalzone"]
+console.log(arrayComidas)
+console.log("Essas são minhas comida favoritas:", "\n" + arrayComidas[0] + "\n"+ arrayComidas[1] + "\n" + arrayComidas[2] + "\n" + arrayComidas[3] + "\n" + arrayComidas[4] + "\n")
+
+let resposta = prompt("Qual é sua comida favorita:")
+arrayComidas[1] = resposta 
+console.log(arrayComidas)*/
+
+let array = "listaDeTarefas"
+let Tarefas = prompt("Digite três tarefas que realiza em seu dia-dia:")
+listaDeTarefas [Tarefas]
